5f3e58853f3c1a4fb52523555c52f409da91f286
Andy Waite <andyw8@users.noreply.github.com>
2023-02-12 16:35:08 -0500

    Require sorbet-runtime

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require 'sorbet-runtime'
+
 module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
