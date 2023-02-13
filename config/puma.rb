a11fd63247a35706f091940ff2495b5dad0fad53
Andy Waite <andyw8@users.noreply.github.com>
2023-02-12 19:54:56 -0500

    Enable typechecking for Puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,7 @@
-# typed: false
+# typed: true
 # Puma configuration file.
+T.bind(self, Puma::DSL)
+
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
